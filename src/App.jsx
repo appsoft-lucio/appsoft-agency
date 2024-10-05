4acd5a7aa9a50c0f544df21ba56b4933c1b99f82
appsoft-lucio <luciocdesouza@gmail.com>
2024-10-05 18:42:04 -0300

    Tirando as rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,10 @@
 // Importa o React, necessário para criar e usar componentes no React
 import React from "react";
 
-// Importa o Router, Routes e Route do 'react-router-dom' para gerenciamento de rotas na aplicação
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
 // Importa o componente Home, que será uma das rotas disponíveis no app
 import Home from "./pages/Home";
 
 // Define e exporta o componente funcional "App", que é o ponto central da aplicação
 export default function App() {
-  return (
-    // O Router envolve toda a aplicação para habilitar o uso de rotas
-    <Router>
-      {/* Define o conjunto de rotas da aplicação */}
-      <Routes>
-        {/* Define a rota principal ('/') e o componente que será renderizado nessa rota (Home) */}
-        <Route path="/" element={<Home />} />
-
-        {/* Você pode adicionar mais rotas aqui no futuro, se necessário */}
-        {/* Exemplo:
-        <Route path="/sobre" element={<About />} />
-        */}
-      </Routes>
-    </Router>
-  );
+  return <Home></Home>;
 }
