8bc73d7c4fa89ae1ba63c163da1747fd3746810e
appsoft-lucio <luciocdesouza@gmail.com>
2024-10-06 10:26:36 -0300

    Adicionando section

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,19 +1,22 @@
 // Importa o React
 import React from "react";
 
+import "./main.css";
+
 // Define e exporta o componente Main
 export default function Main() {
   return (
     // Usa a classe container do Bootstrap para centralizar o conteúdo
     <main className="container my-5">
-      {/* Título da página */}
-      <h2>Bem-vindo à AppSoft</h2>
-
-      {/* Texto descritivo */}
-      <p>
-        Nós desenvolvemos sites incríveis com Vite e Bootstrap para impulsionar
-        sua presença online.
-      </p>
+      <section id="home">
+        {/* Título da seção */}
+        <h2>Bem-vindo à AppSoft</h2>
+        {/* Texto descritivo */}
+        <p>
+          Nós desenvolvemos sites incríveis com Vite e Bootstrap para
+          impulsionar sua presença online.
+        </p>
+      </section>
     </main>
   );
 }
